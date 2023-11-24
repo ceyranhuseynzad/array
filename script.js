9a90070f1684325596e5f401c45fa3c460918710
ceyranhuseynzad <ceyranhuseynzad@gmail.com>
2023-11-24 10:13:25 +0400

    ss

---
+++ script.js
@@ -0,0 +1,52 @@
+const countries=['Finland','Sweden','Denmark']
+const names=['asabeneh','mathias','elias']
+const numbers=[1,2,3,4,5,6,7]
+const products=[
+    {product:'banana',price:3},
+    {product:'mango',price:6},
+    {product:'potato',price:''}
+]
+// 3countries.forEach((element) => console.log(element));
+// 4names.forEach((element) => console.log(element));
+// 5numbers.forEach((element) => console.log(element));
+
+
+// const upper = countries.map(element => {
+//   return element.toUpperCase();
+// });
+
+
+// console.log(upper);
+// 7
+// const upper = countries.map(element => {
+//   return element.length;
+//  });
+
+
+//  console.log(upper);
+//8
+
+
+// Pass a function to map
+// const map1 = numbers.map((x) => x ** 2);
+
+// console.log(map1);
+//9
+//  const upper = names.map(element => {
+//   return element.toUpperCase();
+//  });
+
+
+//  console.log(upper);
+//10
+
+
+
+
+
+
+
+
+
+
+
